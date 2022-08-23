87b24c7596ff8d4a5403212ae3f59208edb50957
Samarinand <samarinand@gmail.com>
2022-08-23 12:33:07 +0300

    n

---
+++ js/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+//# sourceMappingURL=script.dev.js.map
