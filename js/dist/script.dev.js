0afa4d805f79d346d0d18abc9de06410adab2692
Samarinand <samarinand@gmail.com>
2022-08-24 18:36:32 +0300

    ne

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,9 +1,32 @@
 "use strict";
 
-$(document).ready(function () {
-  $('.header__burger').click(function (event) {
-    $('.header__burger, .header__menu').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
+var hamb = document.querySelector("#hamb");
+var popup = document.querySelector("#popup");
+var menu = document.querySelector("#menu").cloneNode(1);
+var body = document.body;
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+} // Код для закрытия меню при нажатии на ссылку
+
+
+var links = Array.from(menu.children);
+links.forEach(function (link) {
+  link.addEventListener("click", closeOnClick);
 });
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
 //# sourceMappingURL=script.dev.js.map
